7600257b408287c60fb42c2ea4f7c2ef55969c39
kubwimainoak <kubwimanajb@gmail.com>
2025-04-25 03:59:55 -0400

    check

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-    basePath: process.env.NODE_ENV === 'production' ? '/fitness' : '',
-    images: {
-      unoptimized: true,
-    },
-  }
-  
-export default nextConfig
+const nextConfig = {};
+
+export default nextConfig;
